662641b2bcc012ff785d17efd7366357c60e8198
Alexandru Bolboaca <alexboly@gmail.com>
2021-10-31 12:47:37 +0200

    Display DKIM and SPF errors using xmllint

--- reportErrors.sh
+++ reportErrors.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p input 
-unzip "samples/*.zip" -d input/
+#mkdir -p input 
+#unzip "samples/*.zip" -d input/
 
+echo "SPF Errors"
+xmllint --xpath "/feedback/record/auth_results/spf[contains(result, 'fail')]" input/*.xml
+
+echo "DKIM Errors"
+xmllint --xpath "/feedback/record/auth_results/dkim[contains(result, 'fail')]" input/*.xml
