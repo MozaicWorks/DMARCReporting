cbc4fc64613580662fd517d0c3cc6a65b5b95c30
Alexandru Bolboaca <alexboly@gmail.com>
2021-10-31 13:07:39 +0200

    Separate location for output reports, make script work repeatedly

--- reportErrors.sh
+++ reportErrors.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-#mkdir -p input 
-#unzip "samples/*.zip" -d input/
+mkdir -p input 
+unzip -o "samples/*.zip" -d input/
 
-echo "SPF Errors"
-xmllint --xpath "/feedback/record/auth_results/spf[contains(result, 'fail')]" input/*.xml
+mkdir -p reports
 
-echo "DKIM Errors"
-xmllint --xpath "/feedback/record/auth_results/dkim[contains(result, 'fail')]" input/*.xml
+xmllint --xpath "/feedback/record/auth_results/spf[contains(result, 'fail')]" input/*.xml >> reports/report-spf.xml
+xmllint --xpath "/feedback/record/auth_results/dkim[contains(result, 'fail')]" input/*.xml >> reports/report-dkim.xml
