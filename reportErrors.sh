c0ad09d094c72e53659b744bb37c5305f09ca8a6
Thierry de Pauw <thierry@thinkinglabs.io>
2021-12-10 23:30:57 +0100

    Call python DMARCReporting from reportErrors.sh (#2)

--- reportErrors.sh
+++ reportErrors.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+readonly SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 mkdir -p input 
 unzip -o "samples/*.zip" -d input/
 for f in samples/*.gz; do gunzip -c $f > input/$(basename ${f%.*}); done
@@ -11,3 +13,5 @@ xmllint --xpath "/feedback/record/auth_results/dkim[contains(result, 'fail')]" i
 
 xmllint --xpath "/feedback/record/row/source_ip[../policy_evaluated/disposition = 'reject']" input/*.xml >>reports/dmarc-rejected.xml
 xmllint --xpath "/feedback/record/row/source_ip[../policy_evaluated/disposition = 'quarantine']" input/*.xml >>reports/dmarc-quarantined.xml
+
+${SCRIPT_DIR}/bin/DMARCReporting ./input
