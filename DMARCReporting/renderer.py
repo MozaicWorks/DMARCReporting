2261a71d30c9b98f56eb69d7d073d17c2b269fe9
Thierry de Pauw <thierry@thinkinglabs.io>
2021-12-10 23:16:52 +0100

    Render nothing on the console when the data is empty (#2)

--- DMARCReporting/renderer.py
+++ DMARCReporting/renderer.py
@@ -4,6 +4,9 @@ from tabulate import tabulate
 
 class ConsoleRenderer():
     def render(self, data):
+        if len(data) == 0:
+            return
+
         print(tabulate(data, headers=[
             "Source IP",
             "DMARC",
--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -5,13 +5,36 @@ from .context import DMARCReporting  # noqa F401
 from DMARCReporting.renderer import ConsoleRenderer
 
 
-def test_render():
+def test_render_when_no_data():
     try:
         output = io.StringIO()
         sys.stdout = output
 
+        data = []
+
         sut = ConsoleRenderer()
-        sut.render([["source_ip", "dmarc_disposition", "dkim_aligned", "spf_aligned"]])
+        sut.render(data)
+
+        expected = ""
+        actual = output.getvalue()
+
+        assert expected == actual
+
+    finally:
+        sys.stdout = sys.__stdout__
+
+
+def test_render_when_data():
+    try:
+        output = io.StringIO()
+        sys.stdout = output
+
+        data = [
+            ["source_ip", "dmarc_disposition", "dkim_aligned", "spf_aligned"]
+        ]
+
+        sut = ConsoleRenderer()
+        sut.render(data)
 
         expected = (
             "Source IP    DMARC              DKIM Aligned    SPF Aligned\n"
