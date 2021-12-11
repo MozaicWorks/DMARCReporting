2127315c381d94c3fd419101de20f3a32624b439
Thierry de Pauw <thierry@thinkinglabs.io>
2021-12-11 10:14:51 +0100

    Add an acceptance test (#2)

---
+++ tests/test_acceptance.py
@@ -0,0 +1,29 @@
+import io
+import sys
+
+from .context import DMARCReporting  # noqa F401
+from DMARCReporting.cli import CLI
+
+
+def test_render_when_data():
+    try:
+        output = io.StringIO()
+        sys.stdout = output
+
+        cli = CLI()
+        cli.execute("./data")
+
+        expected = (
+            "\n"
+            "report.xml\n"
+            "Source IP      DMARC    DKIM Aligned    DKIM Authenticated    SPF Aligned    SPF Authenticated\n"
+            "-------------  -------  --------------  --------------------  -------------  -------------------\n"
+            "80.96.161.193  none     pass            pass                  fail           fail\n"
+            "207.90.221.45  none     pass            pass                  fail           pass\n"
+        )
+        actual = output.getvalue()
+
+        assert expected == actual
+
+    finally:
+        sys.stdout = sys.__stdout__
