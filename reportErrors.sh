019ffa9481303fc9d0c19b1e97f0aa7c6d7ce0ae
Thierry de Pauw <thierry@thinkinglabs.io>
2022-02-05 23:38:50 +0100

    Remove xmllint calls (fixes #5)

--- reportErrors.sh
+++ reportErrors.sh
@@ -6,12 +6,4 @@ mkdir -p input
 unzip -o "samples/*.zip" -d input/
 for f in samples/*.gz; do gunzip -c $f > input/$(basename ${f%.*}); done
 
-mkdir -p reports
-
-xmllint --xpath "/feedback/record/auth_results/spf[contains(result, 'fail')]" input/*.xml >> reports/report-spf.xml
-xmllint --xpath "/feedback/record/auth_results/dkim[contains(result, 'fail')]" input/*.xml >> reports/report-dkim.xml
-
-xmllint --xpath "/feedback/record/row/source_ip[../policy_evaluated/disposition = 'reject']" input/*.xml >>reports/dmarc-rejected.xml
-xmllint --xpath "/feedback/record/row/source_ip[../policy_evaluated/disposition = 'quarantine']" input/*.xml >>reports/dmarc-quarantined.xml
-
 ${SCRIPT_DIR}/bin/DMARCReporting ./input
