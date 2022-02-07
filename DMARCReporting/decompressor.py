fd568b464de510c78c3727f600a42e67520aea3d
Thierry de Pauw <thierry@thinkinglabs.io>
2022-02-07 14:04:12 +0100

    Add Zip decompressor (#8)

--- DMARCReporting/decompressor.py
+++ DMARCReporting/decompressor.py
@@ -1,4 +1,5 @@
 import gzip
+import zipfile
 
 
 class DecompressorFactory():
@@ -11,3 +12,11 @@ class GZipDecompressor():
     def decompress(self, gzip_file_path):
         with gzip.open(gzip_file_path, 'rb') as f:
             return f.read()
+
+
+class ZipDecompressor():
+    def decompress(self, zip_file_path):
+        with zipfile.ZipFile(zip_file_path, 'r') as zf:
+            files = zf.infolist()
+            with zf.open(files[0].filename) as f:
+                return f.read()
--- tests/test_decompressor.py
+++ tests/test_decompressor.py
@@ -1,6 +1,7 @@
 from .context import DMARCReporting  # noqa F401
 from DMARCReporting.decompressor import DecompressorFactory
 from DMARCReporting.decompressor import GZipDecompressor
+from DMARCReporting.decompressor import ZipDecompressor
 
 
 def test_factory_return_gzip_decompressor():
@@ -14,4 +15,11 @@ def test_decompress_gzip():
     sut = GZipDecompressor()
     actual = sut.decompress("./tests/data/sample.txt.gz")
 
-    assert "Hello world!" == actual.decode("utf-8")
+    assert "Hello world, gzip!" == actual.decode("utf-8")
+
+
+def test_decompress_zip():
+    sut = ZipDecompressor()
+    actual = sut.decompress("./tests/data/sample.txt.zip")
+
+    assert "Hello world, zip!" == actual.decode("utf-8")
