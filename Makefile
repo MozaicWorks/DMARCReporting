9e4913c2d5e910ce5e274768769afd8952743309
Thierry de Pauw <thierry@thinkinglabs.io>
2022-02-07 20:59:56 +0100

    Configure PyPi packaging (#11)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 .DEFAULT_GOAL := help
-.PHONY: help install install-dev lint test
+.PHONY: help install install-dev install-build lint test dist
 
 help: ## Print the help documentation
 	@grep -h -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
@@ -11,8 +11,16 @@ install: ## Install runtime dependencies
 install-dev: install ## Install development dependencies
 	pip install -r requirements-dev.txt
 
+install-build: ## Install build dependencies
+	pip install -r requirements-build.txt
+
 lint: ## Check compliance with the style guide
 	flake8
 
 test: lint ## Run unit tests
 	pytest -vv
+
+dist: ## Creates a source distribution and wheel distribution
+	python setup.py sdist bdist_wheel
+	twine check ./dist/*
+	check-wheel-contents dist/*.whl
---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="DMARCReporting",
+    version="0.1.0",
+    description="Simple tool to extract error reports from DMARC files",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MozaicWorks/DMARCReporting",
+    author="Mozaic Works",
+    author_email="alex.bolboaca@mozaicworks.com",
+    license="LICENSE",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["lxml", "tabulate"],
+    python_requires='>=3.8',
+    scripts=["bin/DMARCReporting"],
+)
