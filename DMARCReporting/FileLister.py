30723bace1a619e461553456b1e2112c86eec183
Alex Bolboaca <alexboly@gmail.com>
2022-08-15 19:04:24 +0300

    Clarified private method using python convention

--- DMARCReporting/FileLister.py
+++ DMARCReporting/FileLister.py
@@ -8,8 +8,8 @@ import pathlib
 class FileLister():
     def list(self, input_dir):
         return sorted([
-            f for f in listdir(input_dir) if self.is_zip_or_gz_file(input_dir, f)
+            f for f in listdir(input_dir) if self._is_zip_or_gz_file(input_dir, f)
         ])
 
-    def is_zip_or_gz_file(self, input_dir, file_name):
+    def _is_zip_or_gz_file(self, input_dir, file_name):
         return isfile(join(input_dir, file_name)) and pathlib.Path(file_name).suffix in [".zip", ".gz"]
