5f30d99ed3a2f358a19e108b9041eb1c4934ffb0
Beda Kosata <beda.kosata@gmail.com>
2022-10-20 16:10:29 +0200

    implement output into one large table; add count and source file columns; add optional CSV output; make it possible to show successful records as well

--- DMARCReporting/__init__.py
+++ DMARCReporting/__init__.py
@@ -1,9 +1,28 @@
 __version__ = "0.1.1"
 
+import argparse
 import sys
 from DMARCReporting.cli import CLI
 
 
 def main():
     cli = CLI()
-    cli.execute(sys.argv[1])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "source_dir", type=str, help="directory containing dmarc reports as .zip files"
+    )
+    parser.add_argument(
+        "-c", dest="csv_output_file", type=str, help="csv output file", default=None
+    )
+    parser.add_argument(
+        "-a",
+        dest="get_all",
+        action="store_true",
+        help="process all records - including successful",
+    )
+    args = parser.parse_args()
+
+    cli.execute(
+        args.source_dir, csv_output_file=args.csv_output_file, show_all=args.get_all
+    )
--- DMARCReporting/cli.py
+++ DMARCReporting/cli.py
@@ -4,20 +4,26 @@ import io
 
 from DMARCReporting.decompressor import DecompressorFactory
 from DMARCReporting.parser import DMARCRuaParser
-from DMARCReporting.renderer import ConsoleRenderer
+from DMARCReporting.renderer import ConsoleRenderer, CSVRenderer
 from DMARCReporting.dns import DNS
 from DMARCReporting.FileLister import FileLister
 
 
 class CLI():
-    def execute(self, input_dir):
+    def execute(self, input_dir, csv_output_file=None, show_all=False):
         files = FileLister().list(input_dir)
 
         parser = DMARCRuaParser(DNS())
         renderer = ConsoleRenderer()
+        all_data = []
 
         for file in files:
             file_path = join(input_dir, file)
             report = DecompressorFactory.create(file_path).decompress(file_path)
-            data = parser.parse(io.BytesIO(report))
-            renderer.render(file, data)
+            data = parser.parse(io.BytesIO(report), include_all=show_all)
+            all_data += [[*row, file] for row in data]
+        renderer.render("All", all_data)
+
+        if csv_output_file:
+            renderer = CSVRenderer()
+            renderer.render(csv_output_file, all_data)
