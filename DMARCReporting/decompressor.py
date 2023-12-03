1cdd461700e5595b3aff923164e577d7fb069f39
Thierry de Pauw <thierry@thinkinglabs.io>
2023-12-03 16:08:04 +0100

    Extract decompression with zlib into GZipDecompressor._decompress_with_zlib (#18)

--- DMARCReporting/decompressor.py
+++ DMARCReporting/decompressor.py
@@ -18,12 +18,15 @@ class GZipDecompressor():
             with gzip.open(gzip_file_path, 'rb') as f:
                 return f.read()
         except gzip.BadGzipFile:
-            with open(gzip_file_path, 'rb') as f:
-                do = zlib.decompressobj(wbits=31)
-                data = do.decompress(f.read())
-                remainder = do.unused_data
-                f.seek(f.tell() - len(remainder))
-                return data
+            return self._decompress_with_zlib(gzip_file_path)
+
+    def _decompress_with_zlib(self, gzip_file_path):
+        with open(gzip_file_path, 'rb') as f:
+            do = zlib.decompressobj(wbits=31)
+            data = do.decompress(f.read())
+            remainder = do.unused_data
+            f.seek(f.tell() - len(remainder))
+            return data
 
 
 class ZipDecompressor():
